05a1d8da4b83f5d34448aa6fc28a0fa5721b38e5
Jonathan <jonathandajome@gmail.com>
2023-03-06 19:20:20 -0500

    add formData object

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", function handleSubmit(e) {
+const $form = document.getElementById("form");
+const $buttonMail = document.getElementById("portfolio");
+
+$form.addEventListener("submit", handleSubmit)
+
+function handleSubmit (e) {
   e.preventDefault();
-});
+  const form = new FormData(this);
+  $buttonMail.setAttribute('href', `mailto:jonathandajome@gmail.com?subject=${form.get('name')} &body=${form.get('message')}`)
+  $buttonMail.click()
+}
