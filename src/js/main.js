354ce7d1c45c7cc7895659d4470b19d56a9268bd
Jonathan <jonathandajome@gmail.com>
2023-03-07 13:19:26 -0500

    add mousemove evet

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,39 @@
 const $form = document.getElementById("form");
 const $buttonMail = document.getElementById("portfolio");
 
-$form.addEventListener("submit", handleSubmit)
+$form.addEventListener("submit", handleSubmit);
 
-function handleSubmit (e) {
+function handleSubmit(e) {
   e.preventDefault();
   const form = new FormData(this);
-  $buttonMail.setAttribute('href', `mailto:jonathandajome@gmail.com?subject=${form.get('name')} &body=${form.get('message')}`)
-  $buttonMail.click()
+  $buttonMail.setAttribute(
+    "href",
+    `mailto:jonathandajome@gmail.com?subject=${form.get(
+      "name"
+    )} &body=${form.get("message")}`
+  );
+  $buttonMail.click();
 }
+
+/**
+ * * Mousemove Event
+ */
+
+const $profile = document.getElementById("profile");
+const height = $profile.clientHeight;
+const width = $profile.clientWidth;
+
+$profile.addEventListener("mousemove", (evt) => {
+  const { layerX, layerY } = evt;
+
+  const yRotation = ((layerX - width / 2) / width) * 20;
+  const xRotation = ((layerY - height / 2) / height) * 20;
+
+  const string = `
+  perspective(500px)
+  scale(1.1)
+  rotateX(${xRotation}deg)
+  rotateY(${yRotation}deg)
+  `;
+  $profile.style.transform = string;
+});
